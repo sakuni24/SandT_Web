9fca9aa325895650bfbd800f2d9960479492e642
piyumaldk <piyumaldk@gmail.com>
2020-07-07 03:46:31 +0530

    Validations

--- src/Pages/Admin/Events/AddEvents.js
+++ src/Pages/Admin/Events/AddEvents.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from 'axios';
-import {Col, Row, Alert, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import {Spinner, Col, Row, Alert, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import AdminNav from "../../../Components/AdminNav.component";
-
+import Logo from "../../../Images/logo.jpg";
 export default class Dashboard extends Component {
 
     constructor(props) {
@@ -10,19 +10,14 @@ export default class Dashboard extends Component {
         this.onSubmit = this.onSubmit.bind(this);
     
         this.state = {
+            loading: false,
             alert: false,
-
+            alertMsg: "",
             name: "",
             date: "",
             venue: "",
             description: "",
             headCount: "",
-
-            nameError: "",
-            dateError: "",
-            venueError: "",
-            descriptionError: "",
-            headCountError: "",
         };
     }
 
@@ -37,63 +32,50 @@ export default class Dashboard extends Component {
     reset = e => {
         this.setState({
             alert: false,
-
+            alertMsg: "",
             name: "",
             date: "",
             venue: "",
             description: "",
             headCount: "",
 
-            nameError: "",
-            dateError: "",
-            venueError: "",
-            descriptionError: "",
-            headCountError: "",
         });
         document.getElementById("form").reset();
     };
 
     validate = () => {
         let error = false;
-        const errors = {
-          nameError: "",
-          dateError: "",
-          venueError: "",
-          descriptionError: "",
-          headCountError: ""
-        };
-    
+        let alertMsg = "";
         if (this.state.name.length < 1) {
-          error = true;
-          errors.nameError = 'First name required *'
+            error = true;
+            alertMsg = "Name can't be empty";
         }
         if (this.state.venue.length < 1) {
             error = true;
-            errors.venueError = 'First name required *'
+            alertMsg = "Vanue can't be empty";
         }
         if (this.state.description.length < 1) {
             error = true;
-            errors.descriptionError = 'First name required *'
+            alertMsg = "Description can't be empty";
         }
         if (this.state.headCount.length < 1) {
             error = true;
-            errors.headCountError = 'First name required *'
+            alertMsg = "Count can't be empty";
         }
         
     
-        this.setState({
-          ...this.state,
-          ...errors
-        })
+        this.setState({alertMsg: alertMsg});
         return error;  
     }
 
     onSubmit(e) {
         e.preventDefault();
         const error = this.validate();
-    
+        this.setState({ 
+            loading: true,
+            alert: false
+        });
         if(!error){
-
             const obj = {
                 name: this.state.name,
                 date: this.state.date,
@@ -106,16 +88,25 @@ export default class Dashboard extends Component {
             axios.post("http://localhost:8080/addEvent", obj)
                 .then((res) => {
                     console.log("done");
-                    console.log(res.data);
+                    this.setState({ alert: false });
+                    this.reset();
+                    window.location.reload(false);
                 })
                 .catch((error) => {
-                console.log(error);
+                    console.log(error);
+                    this.setState({
+                        alertMsg: "Server is under maintanace, please try again later!",
+                        alert: true,
+                        loading: false
+                    });
                 });
-                this.setState({ alert: false });
-                this.reset();
+            //        
         }
         else{
-            this.setState({ alert: true });
+            this.setState({ 
+                alert: true,
+                loading: false 
+            });
         }     
     }
     
@@ -123,49 +114,56 @@ export default class Dashboard extends Component {
         return (
             <React.Fragment>
                 <AdminNav/>
-                <Col sm="12" md={{ size: 6, offset: 3 }}>
-                
-                
-                    <Form id="form" onSubmit={this.onSubmit}>
-                        <FormGroup>
-                            <Label for="name">Event Name</Label>
-                            <Input type="text" name="name" id="name" placeholder="Astro" onChange={this.onChange}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="date">Date</Label>
-                            <Input type="text" name="date" id="date" placeholder="2020-01-01" onChange={this.onChange}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="venue">Venue</Label>
-                            <Input type="text" name="venue" id="venue" placeholder="Colombo" onChange={this.onChange}/>
-                        </FormGroup>           
-                        <FormGroup>
-                            <Label for="description">Description</Label>
-                            <Input type="textarea" name="description" id="description" onChange={this.onChange}/>
-                        </FormGroup>
-                        <FormGroup>
-                            <Label for="headCount">Head Count</Label>
-                            <Input type="number" name="headCount" id="headCount" placeholder="25" onChange={this.onChange}/>
-                        </FormGroup>
-                        { this.state.alert ?
-                            <Alert color="danger" status={this.state.alert}>
-                                Fields Can't be empty!
-                            </Alert>
-                        : null}
+                    <center>
+                    <Col xs="6">  
+                    <br/><br/><br/><br/>
+                        <center>
+                            <img src={Logo} alt="S & T Group" style={{justifyContent: 'center',alignItems: 'center',}}/>
+                            
+                                <h4>S & T Group</h4>
+                                Add a new event
+                           
+                        </center>
+                    </Col>
 
-                        <Row> 
-                            <Col sm={{ offset: 1 }}>
+                    <Col xs="5">
+                        <br/>
+                        <Col>
+                            <Form id="form" onSubmit={this.onSubmit}>
+                                <FormGroup>
+                                    <Label for="name">Event Name</Label>
+                                    <Input type="text" name="name" id="name" placeholder="Astro" onChange={this.onChange}/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="date">Date</Label>
+                                    <Input type="text" name="date" id="date" placeholder="2020-01-01" onChange={this.onChange}/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="venue">Venue</Label>
+                                    <Input type="text" name="venue" id="venue" placeholder="Colombo" onChange={this.onChange}/>
+                                </FormGroup>           
+                                <FormGroup>
+                                    <Label for="description">Description</Label>
+                                    <Input type="textarea" name="description" id="description" onChange={this.onChange}/>
+                                </FormGroup>
+                                <FormGroup>
+                                    <Label for="headCount">Head Count</Label>
+                                    <Input type="number" name="headCount" id="headCount" placeholder="25" onChange={this.onChange}/>
+                                </FormGroup>
+                                { this.state.loading ?
+                                    <Spinner animation="border" className="spinner2" alignItems="center"/>
+                                : null}
+                                { this.state.alert ?
+                                    <Alert color="danger" status={this.state.alert}>
+                                        {this.state.alertMsg}
+                                    </Alert>
+                                : null}
                                 <Button color="info" onClick={this.reset}>Reset</Button>
-                            </Col>
-                            <Col sm={{ offset: 1 }}>
-                                <Button color="info" type="submit">Add</Button>
-                            </Col>
-                        </Row>
-                    </Form>
-                
-                </Col>
-                
-                
+                                <Button color="info" type="submit">Add</Button>    
+                            </Form>
+                        </Col>
+                    </Col>
+                    </center>
             </React.Fragment>
           );
         }  
